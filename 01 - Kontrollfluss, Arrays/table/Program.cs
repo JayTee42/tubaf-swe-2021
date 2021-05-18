ef5a7888662613d1a8a256b51365f73b244c6614
Jonas Treumer <endoplasmaticreticulum@yahoo.de>
2021-05-18 13:23:06 +0200

    Added some templates for 01

---
+++ 01 - Kontrollfluss, Arrays/table/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Parse input from terminal:
+        double a, e, s;
+
+        try
+        {
+            Console.Write("Interval start -> ");
+            a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Interval end -> ");
+            e = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Step size -> ");
+            s = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine("Overflow!");
+            return;
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Bad format!");
+            return;
+        }
+
+        // TODO:
+        // Some sanity checks
+
+        // Print the table:
+        Console.WriteLine("     x     |     y     ");
+        Console.WriteLine("-----------------------");
+
+        // TODO
+    }
+}
