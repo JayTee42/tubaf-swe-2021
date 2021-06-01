76a1c52456fef087a71bdd8c3f49f8e58521a69d
Jonas Treumer <endoplasmaticreticulum@yahoo.de>
2021-06-01 15:15:38 +0200

    Proper documentation for IPTools

--- 04 - Objektorientierte Programmierung II/computer/IPTools.cs
+++ 04 - Objektorientierte Programmierung II/computer/IPTools.cs
@@ -7,6 +7,7 @@ static class IPTools
 
     public static string IPAddressToString(byte[] address)
     {
+        // Decide if we have a v4 (four bytes) or a v6 (six bytes) address:
         if (address.Length == 4)
         {
             return IPv4AddressToString(address);
@@ -23,6 +24,7 @@ static class IPTools
 
     public static byte[] StringToIPAddress(string value)
     {
+        // Decide if we have a v4 (contains '.') or a v6 (contains ':') address:
         if (value.Contains('.'))
         {
             return StringToIPv4Address(value);
@@ -39,11 +41,13 @@ static class IPTools
 
     private static string IPv4AddressToString(byte[] address)
     {
+        // Simply use a format string:
         return $"{ address[0] }.{ address[1] }.{ address[2] }.{ address[3] }";
     }
 
     private static byte[] StringToIPv4Address(string value)
     {
+        // Split at all occurrences of '.':
         var comps = value.Split('.');
         var result = new byte[4];
 
@@ -52,6 +56,7 @@ static class IPTools
             throw new ArgumentException("Expected four IPv4 components!");
         }
 
+        // Parse all four components (base 10):
         for (int i = 0; i < 4; i++)
         {
             result[i] = byte.Parse(comps[i]);
@@ -62,6 +67,8 @@ static class IPTools
 
     private static string IPv6AddressToString(byte[] address)
     {
+        // Build a string array where each entry
+        // holds the hexstring concatenation of two address bytes:
         var comps = new string[8];
 
         for (int i = 0; i < 8; i++)
@@ -69,11 +76,13 @@ static class IPTools
             comps[i] = address[2 * i].ToString("x2") + address[(2 * i) + 1].ToString("x2");
         }
 
+        // Connect all eight components using ':' as separator:
         return string.Join(":", comps);
     }
 
     private static byte[] StringToIPv6Address(string value)
     {
+        // Split at all occurrences of ':':
         var comps = value.Split(':');
         var result = new byte[16];
 
@@ -84,6 +93,8 @@ static class IPTools
 
         for (int i = 0; i < 8; i++)
         {
+            // Split the four digits into two substrings
+            // where each one represents one address byte:
             var comp = comps[i];
 
             if (comp.Length != 4)
@@ -93,6 +104,7 @@ static class IPTools
 
             var substrings = new string[] { comp.Substring(0, 2), comp.Substring(2) };
 
+            // Parse the two address bytes with hex specifier:
             for (int j = 0; j < 2; j++)
             {
                 result[(2 * i) + j] = byte.Parse(substrings[j], NumberStyles.AllowHexSpecifier);
