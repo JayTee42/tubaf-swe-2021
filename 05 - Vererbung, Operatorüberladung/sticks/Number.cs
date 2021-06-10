58be4ceef6049c809d7d8f3e604092c99a918c5d
Jonas Treumer <endoplasmaticreticulum@yahoo.de>
2021-06-10 15:18:45 +0200

    Sticks

---
+++ 05 - Vererbung, Operatorüberladung/sticks/Number.cs
@@ -0,0 +1,58 @@
+using System;
+
+class Number
+{
+    // Actual number representation
+    private uint _val;
+
+    // Symbol representation
+    private static readonly char NumChar = '|';
+
+    // Public access via symbolic string
+    public string NumberSticks
+    {
+        get => new String(NumChar, (int)_val);
+
+        set
+        {
+            // Validation:
+            foreach (var c in value)
+            {
+                if (c != NumChar)
+                {
+                    throw new FormatException("Invalid character!");
+                }
+            }
+
+            _val = (uint)value.Length;
+        }
+    }
+
+    // Constructors:
+    public Number(uint val) => _val = val;
+    public Number(string numberSticks) => NumberSticks = numberSticks;
+
+    // Method overrides from System.Object:
+    public override string ToString() => NumberSticks;
+
+    public override bool Equals(object obj)
+    {
+        var other = obj as Number;
+        return (other == null) ? false : (this == other);
+    }
+
+    // 1.) Gleiche Objekte => gleicher Hashcode
+    // 2.) Ungleiche Objekte => möglichst ungleicher Hashcode
+    public override int GetHashCode() => (int)_val;
+
+    // Operator overloading:
+    public static Number operator +(Number lhs, Number rhs) => new Number(lhs._val + rhs._val);
+    public static Number operator -(Number lhs, Number rhs) => new Number((lhs._val < rhs._val) ? 0 : lhs._val - rhs._val);
+    public static Number operator *(Number lhs, Number rhs) => new Number(lhs._val * rhs._val);
+    public static Number operator /(Number lhs, Number rhs) => new Number(lhs._val / rhs._val);
+
+    public static bool operator ==(Number lhs, Number rhs) => lhs?._val == rhs?._val;
+    public static bool operator !=(Number lhs, Number rhs) => !(lhs == rhs);
+    public static bool operator <(Number lhs, Number rhs) => lhs._val < rhs._val;
+    public static bool operator >(Number lhs, Number rhs) => lhs._val > rhs._val;
+}
---
+++ 05 - Vererbung, Operatorüberladung/sticks/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Number n1 = new Number(25);
+        Number n2 = new Number(25);
+
+        Console.WriteLine($"n1 = { n1 }, n2 = { n2 }");
+
+        var res = n1.Equals(n2);
+        Console.WriteLine($"Equality: { res }");
+
+        Console.WriteLine("Hello!".GetHashCode());
+    }
+}
