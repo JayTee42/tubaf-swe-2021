eadb89b5796f3b7a62d2a24e48529ed4f2482fa1
Jonas Treumer <endoplasmaticreticulum@yahoo.de>
2021-06-29 14:44:36 +0200

    Added Csv reading method

---
+++ 07 - Modellierung, Deserialisierung, Tests/phonesim/Csv.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+static class CsvReader
+{
+    public static Dictionary<string, string>[] ReadCsv(string path)
+    {
+        // Read all the lines from the given file:
+        var lines = File.ReadAllLines(path);
+
+        // We need at least one line which is the header:
+        if (lines.Length == 0)
+        {
+            throw new FormatException("Need CSV header line.");
+        }
+
+        // Retrieve the keys from the header line and normalize them:
+        var keys = lines[0].Split(',');
+
+        for (int i = 0; i < keys.Length; i++)
+        {
+            keys[i] = keys[i].Trim().ToLower();
+        }
+
+        // Create the array of dictionaries:
+        var dicts = new Dictionary<string, string>[lines.Length - 1];
+
+        // Fill it row by row (skipping the header):
+        for (int i = 1; i < lines.Length; i++)
+        {
+            // Split the line and collect the trimmed values.
+            // Note: This approach cannot handle commas in string!
+            var values = lines[i].Split(',');
+
+            if (keys.Length != values.Length)
+            {
+                throw new FormatException($"CSV file has { keys.Length } columns, but line has { values.Length } values.");
+            }
+
+            // Create and fill a dictionary for the current line:
+            var dict = new Dictionary<string, string>(keys.Length);
+
+            for (int j = 0; j < keys.Length; j++)
+            {
+                dict[keys[j]] = values[j].Trim();
+            }
+
+            // Store the dictionary in our array:
+            dicts[i - 1] = dict;
+        }
+
+        return dicts;
+    }
+}
+
+static class CsvWriter
+{
+
+}
--- 07 - Modellierung, Deserialisierung, Tests/phonesim/Simulator.cs
+++ 07 - Modellierung, Deserialisierung, Tests/phonesim/Simulator.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 class Simulator
 {
-    public Dictionary<string, MobilePhone> Phone { get; }
+    public Dictionary<string, MobilePhone> Phones { get; }
     public Test[] Tests { get; }
 
     public Simulator(string phonePath, string testPath)
@@ -13,6 +13,24 @@ class Simulator
 
     public void RunAllTests(string resultPath)
     {
-        
+        foreach (var test in Tests)
+        {
+            // Execute the test if the preconditions are met:
+            TestResultType resultType;
+
+            if (Phones.TryGetValue(test.PhoneNumber, out var phone))
+            {
+                resultType = test.Run(phone) ? TestResultType.Success : TestResultType.Failure;
+            }
+            else
+            {
+                resultType = TestResultType.PrecoditionFailure;
+            }
+
+            // Construct the test result:
+            var result = new TestResult(test.Id, resultType);
+
+            // TODO: Serialize and write to the CSV file.
+        }
     }
 }
